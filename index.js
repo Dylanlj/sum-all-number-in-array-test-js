27fd2bc3cfd0c263c0dbe504119c87acc0c9fd29
Dylanlj <dylanl-j@hotmail.ca>
2018-03-14 23:10:17 +0000

    finished exercise

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  for (let index of array){
+
+    if(Array.isArray(index)){
+      index = sumItems(index)
+    }
+    sum += index
+  }
+  return sum
 }
 
 module.exports = sumItems;
+
